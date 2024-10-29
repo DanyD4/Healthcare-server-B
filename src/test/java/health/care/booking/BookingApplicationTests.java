ab6399d330e334d1028a3b5eea02bf8cc291f21f
Dany Ramirez <dany.ramirez.92@hotmail.com>
2024-10-29 21:23:30 +0100

    inget fungerar

--- src/test/java/health/care/booking/BookingApplicationTests.java
+++ src/test/java/health/care/booking/BookingApplicationTests.java
@@ -2,10 +2,8 @@ package health.care.booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class BookingApplicationTests {
 
 	@Test
