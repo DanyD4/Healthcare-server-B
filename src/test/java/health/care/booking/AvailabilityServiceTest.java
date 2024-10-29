4a6e6b5bab10fd43417f3615722317088e6c9d46
Dany Ramirez <dany.ramirez.92@hotmail.com>
2024-10-29 20:23:42 +0100

    test6

--- src/test/java/health/care/booking/AvailabilityServiceTest.java
+++ src/test/java/health/care/booking/AvailabilityServiceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-@SpringBootTest
+
 public class AvailabilityServiceTest {
 
     @Mock
